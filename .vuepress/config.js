0a49a82ec271518f8aeb004d43dae7bc254e04db
Souma Suzuki <suzuki.souma@hrbrain.co.jp>
2018-09-12 15:12:58 +0900

    feat: Use vuepress

---
+++ .vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: 'やるぞReact 作るぞSPA',
+  description: 'これを読めばReactがデキるようになる（かもしれない）',
+  themeConfig: {
+    sidebarDepth: 2,
+    displayAllHeaders: true,
+    sidebar: 'auto'
+  }
+}
--- 03. Learn Basics/countShari.js
+++
@@ -1,6 +0,0 @@
-function countShari(s) {
-  // ... Write your code here.
-}
-
-var sushis = ['イカ', 'シャリ', 'カンパチ', 'イカ天', 'シャリ', 'イカ', 'シャリ']
-console.log(contShari(sushis)) // -> 3
